b24b6dfdaf7d8ef56786b39ee79b4a651f404366
DashaMelnik1 <dashamakaruk82@gmail.com>
2023-12-21 23:32:07 +0300

    Update script.js
    Написала код в JS, сделала все задания

--- script.js
+++ script.js
@@ -7,3 +7,33 @@
 /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
 
 /* Очищать input после добавления нового элемента в список */
+
+const input = document.querySelector('#input');
+const items = document.querySelector('.items');
+
+function addItem() {
+    const itemText = input.value;
+
+    const listItem = document.createElement('div');
+
+    listItem.textContent = itemText;
+
+    listItem.addEventListener('click', function() {
+        listItem.classList.toggle('done');
+    })
+
+    if (itemText != '') {
+        items.append(listItem);
+    }
+
+    input.value = '';
+
+}
+
+function sendByKey(event) {
+    if (event.key == 'Enter') {
+        addItem();
+    }
+}
+
+input.addEventListener('keydown', sendByKey);
